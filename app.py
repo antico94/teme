ed84b77787f150b183db06b36a71ab3ef22b8ed9
Meiu <meiuandrei@gmail.com>
2021-04-06 16:27:38 +0300

    halfway there

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
 import data
 
-# put your code here
+import dotenv
+from flask import Flask, render_template, request, redirect, url_for
+from data_manager import *
+
+
+dotenv.load_dotenv()
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return redirect(url_for("login"))
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
